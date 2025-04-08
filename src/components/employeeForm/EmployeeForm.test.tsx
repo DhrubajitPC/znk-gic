4d6fcf5d53799bc7d2219b11b59b5791e1ebed77
Dhrubajit <dhrubajit1992@gmail.com>
2025-04-08 15:20:22 +0800

    update employee form test

--- src/components/employeeForm/EmployeeForm.test.tsx
+++ src/components/employeeForm/EmployeeForm.test.tsx
@@ -1,9 +1,6 @@
-import { act, fireEvent, render, screen } from "@testing-library/react";
-
-import dayjs from "dayjs";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router";
 import { expect, test, vi } from "vitest";
-import { employeeMock } from "../../fixtures/employeeMock.fixture";
 import { Employee } from "../../types/Employee";
 import { EmployeeForm } from "./EmployeeForm";
 
@@ -60,122 +57,15 @@ test("Displays validation errors for required fields", async () => {
   ).toBeInTheDocument();
 });
 
-test.skip("Validates joined date is after date of birth", async () => {
-  render(component);
-  const firstNameInput = screen.getByLabelText(/first name/i);
-  const lastNameInput = screen.getByLabelText(/last name/i);
-  const emailInput = screen.getByLabelText(/email/i);
-  const phoneNumberInput = screen.getByLabelText(/phone number/i);
-  const dateOfBirthInput = screen.getByLabelText(/date of birth/i);
-  const joinedDateInput = screen.getByLabelText(/joined date/i);
-  const femaleGenderRadio = screen.getByRole("radio", { name: "Female" });
-  const submitButton = screen.getByRole("button", { name: /submit/i });
-
-  act(() => {
-    fireEvent.change(firstNameInput, { target: { value: "Joanathan" } });
-    fireEvent.change(lastNameInput, { target: { value: "Renfield" } });
-    fireEvent.change(emailInput, { target: { value: "john.doe@example.com" } });
-    fireEvent.change(phoneNumberInput, { target: { value: "81234567" } });
-    fireEvent.click(femaleGenderRadio);
-
-    fireEvent.change(dateOfBirthInput, {
-      target: { value: dayjs("1990-01-01") },
-    });
-    fireEvent.blur(dateOfBirthInput);
-    firstNameInput.focus();
-    fireEvent.change(joinedDateInput, {
-      target: { value: dayjs("1920-01-01") },
-    });
-    fireEvent.blur(dateOfBirthInput);
-
-    screen.debug(dateOfBirthInput);
-    screen.debug(joinedDateInput);
-    // fireEvent.change(dateOfBirthInput, employeeData.dateOfBirth);
-    // fireEvent.change(joinedDateInput, employeeData.joinedDate);
-    fireEvent.click(submitButton);
-  });
-
-  expect(await screen.queryByText(/first name is required/i)).toBeFalsy();
-  expect(await screen.queryByText(/last name is required/i)).toBeFalsy();
-  expect(await screen.queryByText(/email is required/i)).toBeFalsy();
-  expect(await screen.queryByText(/phone number is required/i)).toBeFalsy();
-  expect(await screen.queryByText(/gender is required/i)).toBeFalsy();
-  expect(await screen.queryByText(/date of birth is required/i)).toBeFalsy();
-  expect(await screen.queryByText(/joined date is required/i)).toBeFalsy();
-  expect(
-    await screen.queryByText(/joined date must be after date of birth/i)
-  ).toBeInTheDocument();
-});
-
-test.skip("Submits form with valid data", async () => {
-  const mockSubmit = vi.fn();
-
-  const employee = employeeMock[0] as Employee;
+test("Validates joined date is after date of birth", async () => {
   render(
     <BrowserRouter>
-      <EmployeeForm employee={employee} onSubmit={mockSubmit} />
+      <EmployeeForm employee={{ ...employeeData, id: "" }} />
     </BrowserRouter>
   );
-
-  const firstNameInput = screen.getByLabelText(/first name/i);
-  const lastNameInput = screen.getByLabelText(/last name/i);
-  const emailInput = screen.getByLabelText(/email/i);
-  const phoneNumberInput = screen.getByLabelText(/phone number/i);
-  const dateOfBirthInput = screen.getByLabelText(/date of birth/i);
-  const joinedDateInput = screen.getByLabelText(/joined date/i);
-  const femaleGenderRadio = screen.getByRole("radio", { name: "Female" });
-
-  act(() => {
-    fireEvent.change(firstNameInput, { target: { value: "Joanathan" } });
-    fireEvent.change(lastNameInput, { target: { value: "Renfield" } });
-    fireEvent.change(emailInput, { target: { value: "john.doe@example.com" } });
-    fireEvent.change(phoneNumberInput, { target: { value: "81234567" } });
-    fireEvent.click(femaleGenderRadio);
-
-    firstNameInput.focus();
-    fireEvent.change(dateOfBirthInput, { target: { value: "2000-01-01" } });
-    fireEvent.blur(dateOfBirthInput);
-    expect((dateOfBirthInput as HTMLInputElement).value).toBe("2000-01-01");
-
-    joinedDateInput.focus();
-    fireEvent.change(joinedDateInput, { target: { value: "2025-01-01" } });
-    joinedDateInput.blur();
-
-    const submitButton = screen.getByRole("button", { name: /submit/i });
-    fireEvent.click(submitButton);
-  });
-  screen.debug(firstNameInput);
-  screen.debug(lastNameInput);
-  screen.debug(emailInput);
-  screen.debug(phoneNumberInput);
-  screen.debug(femaleGenderRadio);
-  screen.debug(dateOfBirthInput);
-  screen.debug(joinedDateInput);
-
-  expect(screen.queryByText(/first name is required/i)).toBeFalsy();
-  expect(screen.queryByText(/last name is required/i)).toBeFalsy();
-  expect(screen.queryByText(/email is required/i)).toBeFalsy();
-  expect(screen.queryByText(/phone number is required/i)).toBeFalsy();
-  expect(screen.queryByText(/gender is required/i)).toBeFalsy();
-  expect(screen.queryByText(/date of birth is required/i)).toBeFalsy();
-  expect(screen.queryByText(/joined date is required/i)).toBeFalsy();
-
-  // screen.debug(await screen.findByText(/date of birth is required/i));
-  // expect(screen.queryByText(/first name is required/i)).toBeFalsy();
-  // expect(screen.queryByText(/last name is required/i)).toBeFalsy();
-  // expect((dateOfBirthInput as HTMLInputElement).value).toBe("2000-01-01");
-  // expect(screen.queryByText(/date of birth is required/i)).toBeInTheDocument();
-  // expect(screen.queryByText(/joined date is required/i)).toBeFalsy();
-
-  // expect(mockSubmit).toHaveBeenCalled();
-
-  // expect(mockSubmit).toHaveBeenCalledWith({
-  //   firstName: "Jonathan",
-  //   lastName: "Renfield",
-  //   email: "john.doe@example.com",
-  //   phoneNumber: "81234567",
-  //   gender: "male",
-  //   dateOfBirth: "2000-01-01",
-  //   joinedDate: "2025-01-01",
-  // });
+  const submitButton = screen.getByRole("button", { name: /submit/i });
+  fireEvent.click(submitButton);
+  expect(
+    await screen.findByText(/joined date must be after date of birth/i)
+  ).toBeInTheDocument();
 });
