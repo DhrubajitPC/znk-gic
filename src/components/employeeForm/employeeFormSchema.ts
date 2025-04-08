3938f487094c44e0478035c7d4fc4f568fd1a8ae
Dhrubajit <dhrubajit1992@gmail.com>
2025-04-08 14:32:32 +0800

    update error messages

--- src/components/employeeForm/employeeFormSchema.ts
+++ src/components/employeeForm/employeeFormSchema.ts
@@ -1,21 +1,29 @@
-import { z } from "zod";
 import dayjs from "dayjs";
+import { z } from "zod";
 
 export const employeeFormSchema = z
   .object({
     firstName: z
-      .string()
+      .string({
+        required_error: "First name is required",
+      })
       .min(6, "Minimum 6 characters required")
       .max(10, "Maximum 10 characters allowed"),
     lastName: z
-      .string()
+      .string({
+        required_error: "Last name is required",
+      })
       .min(6, "Minimum 6 characters required")
       .max(10, "Maximum 10 characters allowed"),
-    email: z.string().email("Invalid email format"),
+    email: z.string({required_error: "Email is required"}).email("Invalid email format"),
     phoneNumber: z
-      .string()
+      .string({
+        required_error: "Phone number is required",
+      })
       .regex(/^[689]\d{7}$/, "Must be a valid Singaporean phone number"),
-    gender: z.enum(["male", "female"]),
+    gender: z.enum(["male", "female"], {
+      required_error: "Gender is required",
+    }),
     dateOfBirth: z
       .custom<dayjs.Dayjs>()
       .refine((val) => val && val.isBefore(dayjs()), {
